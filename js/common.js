861aa9e74fae0b93a3485d3adeada697820af0e9
G <105860599+po-dzin@users.noreply.github.com>
2026-01-25 13:33:37 +0200

    Update payment URL to Wayforpay

--- js/common.js
+++ js/common.js
@@ -16,8 +16,7 @@ $(".openModal").click(function(e) {
   }
 
   // 3) открываем оплату
-  const payUrl = 'https://www.liqpay.ua/api/3/checkout?data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJhbW91bnQiOiIzNTAiLCJjdXJyZW5jeSI6IlVBSCIsImRlc2NyaXB0aW9uIjoi0J7Qv9C70LDRgtCwINC60YPRgNGB0YMgU2hvcnQgUmVib290INCy0ZbQtCBDZW50ZXJ3YXkiLCJwdWJsaWNfa2V5Ijoic2FuZGJveF9pMjM2MjQ4NTk3OTciLCJsYW5ndWFnZSI6InVrIiwicmVzdWx0X3VybCI6Imh0dHBzOi8vcmVib290LmNlbnRlcndheS5uZXQudWEvdGhhbmtzIn0=&signature=w/oBFIjZX8EDf5+mn4yaiEPpe5A=';
-
+  const payUrl = 'https://secure.wayforpay.com/button/b9904492f03d4';
   window.location.href = payUrl;
 });
 function newDate() {
